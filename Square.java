6731e85b749b4139266e14df38dc5c736e7335bd
vilasp29 <prabhu_vilas@yahoo.com>
2018-05-15 19:18:46 +0000

    Create new file

---
+++ Square.java
@@ -0,0 +1,17 @@
+public class square {
+	int color; // 0 is black and 1 is white
+
+	public static void main(String[] args){
+	}
+
+	public void setColor(int clr) { color = clr; }
+	public int getColor() { return color; }
+
+	public void BoardSquareWhite()
+	{
+	}
+
+	public void BoardSquareBlack()
+	{
+	}
+}
