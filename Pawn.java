107bf0384e843170a06b916d3aa25cf78084a5ff
vilasp29 <prabhu_vilas@yahoo.com>
2018-05-15 19:16:31 +0000

    Create new file

---
+++ Pawn.java
@@ -0,0 +1,21 @@
+public class Pawn extends pieces{
+	public static void SetImagePawn() {
+		String Pawn = "P";
+		System.out.println("" + Pawn);
+	}
+	public static void MoveSquare() {
+		
+	}
+	public static void MoveSquareSetupl() {
+		
+	}
+	public static void KillSquare() {
+		
+	}
+	public static void main (String[] args) {
+		SetImagePawn();
+		MoveSquare();
+		MoveSquareSetupl();
+		KillSquare();
+	}
+}
